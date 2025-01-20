787802756887ad1c2f990a33c59117548886b049
govind <govindmohan2004@gmail.com>
2025-01-20 21:10:40 +0530

    hangman

--- hangman.py
+++ hangman.py
@@ -4,20 +4,30 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-
-guess = input("Guess a letter: ").lower()
-placeholder=""
-word_length=len(chosen_word)
-for i in range(0,word_length):
-    placeholder+="_"
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
 print(placeholder)
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-display=""
-for letter in chosen_word:
-    if letter == guess:
-        display+=letter
-    else:
-        display+="_"
-print(display)
+
+game_over=False
+correct_letters=[]
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    display = ""
+
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display+=letter
+        else:
+            display += "_"
+
+    print(display)
+    if "_" not in display:
+        print("YOU WIN")
+        game_over=True
